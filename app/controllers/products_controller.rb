84242cc47274e8b64d7e4c679d98e13f88246844
acv-tamhc <acv-tamhc@atmarkcafe.org>
2018-01-28 23:18:24 +0700

    check code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,4 @@
 class ProductsController < ApplicationController
-<<<<<<< HEAD
 	before_action :authenticate_model!
 	private def product_params
 		params.require(:product).permit(:title, :description, :price, :category_id)
@@ -10,15 +9,12 @@ class ProductsController < ApplicationController
 	private def get_product_by_id
 		@product = Product.find(product_id)
 	end
-=======
->>>>>>> 7e5a8d5e09942a99f9b0c92305ecebbeca81debf
 	def index
 		@products = Product.all
 	end
 
 	def new
 		if(params[:id])
-<<<<<<< HEAD
 			@product = Product.find(product_id)
     else
       @product = Product.new
@@ -26,16 +22,10 @@ class ProductsController < ApplicationController
 	end
 
 	def create
-=======
 			@product = Product.find(params[:id])
 		else
 			@product = Product.new
 		end
-	end
-
-	def create 
->>>>>>> 7e5a8d5e09942a99f9b0c92305ecebbeca81debf
-		redirect_to products_path
 	end
 
 end
--- spec/features/crud_products_spec.rb
+++ spec/features/crud_products_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
-<<<<<<< HEAD
 describe 'User can CRUD products', type: :feature do
-	# let(:product)
-=======
-describe 'User can CRUD products', type: :feature do 
->>>>>>> 7e5a8d5e09942a99f9b0c92305ecebbeca81debf
 	it 'can create a products' do
 		visit 'products/new'
 		expect(page).to have_link 'Create a product'
@@ -15,12 +10,5 @@ describe 'User can CRUD products', type: :feature do
 		click_on 'Submit'
 		expect(page).to have_content 'jQuery book'
 	end
-<<<<<<< HEAD
 
-	it "can be create products" do
-
-	end
 end
-=======
-end
->>>>>>> 7e5a8d5e09942a99f9b0c92305ecebbeca81debf
